c014c9713f49c6acb65c157ad4b21acdb49274da
yamayamaKo <yamachanwow@gmail.com>
2025-02-07 18:38:39 +0900

    middlewareの追加

--- src/app/api/item/readall/route.ts
+++ src/app/api/item/readall/route.ts
@@ -6,7 +6,6 @@ export async function GET() {
   try {
     await connectDB()
     const allItems = await ItemModel.find()
-    console.log(allItems)
     return NextResponse.json({message: "アイテム読み取り成功（全件）", allItems: allItems})
   } catch {
     return NextResponse.json({message: "アイテム読み取り失敗（全件）"})
---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { jwtVerify } from "jose";
+import { NextResponse } from "next/server";
+
+// appディレクトリと同じ階層に置く
+// 全てのAPIルートのリクエストの前に実行される
+export async function middleware(request: Request) {
+  const token = await request.headers.get("Authorization")?.split("Bearer ")[1]
+
+  if (!token) {
+    return NextResponse.json({ message: "認証エラー" }, { status: 401 })
+  }
+
+  try {
+    // トークンの検証
+    const secretKey = new TextEncoder().encode(process.env.SERVER_KEY)
+    const decodedJwt = await jwtVerify(token, secretKey)
+
+    return NextResponse.next()
+  } catch {
+    return NextResponse.json({ message: "トークンが間違っています" }, { status: 401 })
+  }
+}
+
+export const config = {
+  matcher: [
+    // appディレクトリ以下の指定でOK
+    "/api/item/create",
+    "/api/item/update/:path*",
+    "/api/item/delete/:path*",
+  ]
+}
